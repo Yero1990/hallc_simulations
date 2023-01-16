e50d654de68e68d19e19a1c20f7b4c10afb6c3a4
Carlos Yero <cyero@jlab.org>
2023-01-15 22:13:23 -0500

    updated

--- utils/hist_utils.h
+++ utils/hist_utils.h
@@ -89,7 +89,7 @@ void extract_1d_hist(TH1F *h1, TString xlabel, TString out_fname)
 
 //Brief: This function extracts 2D Histogram Bin Information
 // Useful for extracting yields or cross sections binned in 2D kinematics
-void extract_2d_hist(TH2F *h2, TString xlabel, TString ylabel, TString out_fname)
+void extract_2d_hist(TH2F *h2, TString xlabel, TString ylabel, TString out_fname, Bool_t csv_flg=false)
 {
 
   /*
@@ -127,31 +127,58 @@ void extract_2d_hist(TH2F *h2, TString xlabel, TString ylabel, TString out_fname
 
 
   ofile.open(out_fname.Data());
-
-  TString header = Form("# 2D Histogram Bin Extraction \n"
-			"#                   \n"
-			"# histogram parameters:  \n"
-			"# ybins      = %d   | ylabel: %s \n"
-			"# xbins      = %d   | xlabel: %s \n"
-			"# ybin width = %.3f \n"
-			"# xbin width = %.3f \n"
-			"#                   \n"
-			"# header definitions:\n"
-			"# ib:         (x,y)  bin number \n"
-			"# xb:         x-axis bin number \n"
-			"# yb:         y-axis bin number \n"
-			"# x0:         x-axis central bin value \n"
-			"# xlow:       x-axis low-edge bin value \n"
-			"# xup:        x-axis up-edge bin value \n"
-			"# y0:         y-axis central bin value \n"
-			"# ylow:       y-axis low-edge bin value \n"
-			"# yup:        y-axis up-edge bin value \n"
-			"# zcont:      bin content (z-axis) \n"
-			"# zcont_err:  bin content error (z-axis) \n"
-			"#                                        \n"
-			"#! ib[i,0]/  xb[i,1]/  yb[i,2]/  x0[f,3]/  xlow[f,4]/  xup[f,5]/  y0[f,6]/  ylow[f,7]/  yup[f,8]/  zcont[f,9]/  zcont_err[f,10]/  "
-			,y_Nbins, ylabel.Data(), x_Nbins, xlabel.Data(), ybin_width, xbin_width  );
-
+  TString header;
+  if(csv_flg){
+    header = Form("# 2D Histogram Bin Extraction \n"
+		  "#                   \n"
+		  "# histogram parameters:  \n"
+		  "# ybins      = %d   | ylabel: %s \n"
+		  "# xbins      = %d   | xlabel: %s \n"
+		  "# ybin width = %.3f \n"
+		  "# xbin width = %.3f \n"
+		  "#                   \n"
+		  "# header definitions:\n"
+		  "# ib:         (x,y)  bin number \n"
+		  "# xb:         x-axis bin number \n"
+		  "# yb:         y-axis bin number \n"
+		  "# x0:         x-axis central bin value \n"
+		  "# xlow:       x-axis low-edge bin value \n"
+		  "# xup:        x-axis up-edge bin value \n"
+		  "# y0:         y-axis central bin value \n"
+		  "# ylow:       y-axis low-edge bin value \n"
+		  "# yup:        y-axis up-edge bin value \n"
+		  "# zcont:      bin content (z-axis) \n"
+		  "# zcont_err:  bin content error (z-axis) \n"
+		  "#                                        \n"
+		  "ib, xb, yb,  x0,  xlow,  xup,  y0,  ylow,  yup,  zcont,  zcont_err,  "
+		  ,y_Nbins, ylabel.Data(), x_Nbins, xlabel.Data(), ybin_width, xbin_width  );
+  }
+  else{
+    header = Form("# 2D Histogram Bin Extraction \n"
+		  "#                   \n"
+		  "# histogram parameters:  \n"
+		  "# ybins      = %d   | ylabel: %s \n"
+		  "# xbins      = %d   | xlabel: %s \n"
+		  "# ybin width = %.3f \n"
+		  "# xbin width = %.3f \n"
+		  "#                   \n"
+		  "# header definitions:\n"
+		  "# ib:         (x,y)  bin number \n"
+		  "# xb:         x-axis bin number \n"
+		  "# yb:         y-axis bin number \n"
+		  "# x0:         x-axis central bin value \n"
+		  "# xlow:       x-axis low-edge bin value \n"
+		  "# xup:        x-axis up-edge bin value \n"
+		  "# y0:         y-axis central bin value \n"
+		  "# ylow:       y-axis low-edge bin value \n"
+		  "# yup:        y-axis up-edge bin value \n"
+		  "# zcont:      bin content (z-axis) \n"
+		  "# zcont_err:  bin content error (z-axis) \n"
+		  "#                                        \n"
+		  "#! ib[i,0]/  xb[i,1]/  yb[i,2]/  x0[f,3]/  xlow[f,4]/  xup[f,5]/  y0[f,6]/  ylow[f,7]/  yup[f,8]/  zcont[f,9]/  zcont_err[f,10]/  "
+		  ,y_Nbins, ylabel.Data(), x_Nbins, xlabel.Data(), ybin_width, xbin_width  );
+  }
+  
   //Write header to data file
   ofile << header.Data() << endl;
   
@@ -177,9 +204,12 @@ void extract_2d_hist(TH2F *h2, TString xlabel, TString ylabel, TString out_fname
       xup  = h2->GetXaxis()->GetBinUpEdge(xb);
       x0   = (xlow + xup)/2.; //h2->GetXaxis()->GetBinCenter(xb);
 
-      
-      ofile  << std::setw(7) << ib << std::setw(10) << xb << std::setw(10) << yb << std::setw(14) << x0 << std::setw(10) << xlow << std::setw(10) << xup << std::setw(12) << y0 << std::setw(10) << ylow << std::setw(12) << yup << std::setw(12) << z_cont << std::setw(14) << z_cont_err << endl;
-
+      if(csv_flg){
+	ofile  << Form("%i, %i, %i, %.1f, %.1f, %.1f, %.1f, %.1f, %.1f, %.3E, %.3E \n", ib, xb, yb, x0, xlow, xup, y0, ylow, yup, z_cont, z_cont_err);
+      }
+      else{
+	ofile  << std::setw(7) << ib << std::setw(10) << xb << std::setw(10) << yb << std::setw(14) << x0 << std::setw(10) << xlow << std::setw(10) << xup << std::setw(12) << y0 << std::setw(10) << ylow << std::setw(12) << yup << std::setw(12) << z_cont << std::setw(14) << z_cont_err << endl;
+      }
     }// end x-bins loop
     
   } // end y-bins loop
