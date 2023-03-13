13007530cc1be2e042b049ec888729b5cde35db3
Carlos Yero <cyero002@fiu.edu>
2023-03-12 23:18:54 -0400

    udated

--- kinematics/calc_h2_kin_version2.py
+++ kinematics/calc_h2_kin_version2.py
@@ -206,14 +206,17 @@ if __name__ == "__main__":
     # CaFe Heep elastics to cover range from +5 to +15 % to cover entire MF,SRC range (regardless of 10.3 or 10.6 beam energy)
     # heep @ shms_delta = +5%
     #calc_h2(Ei=10.6, th_e=-1, kf=8.978, e_arm='SHMS', case='case2', verbose=True)
+    calc_h2(Ei=10.549, th_e=6.8, kf=-1, e_arm='SHMS', case='case1', verbose=True) 
 
     # heep @ shms_delta = +10%
     #calc_h2(Ei=10.6, th_e=-1, kf=9.405, e_arm='SHMS', case='case2', verbose=True)
-    
+    calc_h2(Ei=10.549, th_e=7.495, kf=-1, e_arm='SHMS', case='case1', verbose=True)
+
     # heep @ shms_delta = +15%
     #calc_h2(Ei=10.6, th_e=-1, kf=9.833, e_arm='SHMS', case='case2', verbose=True)
+    calc_h2(Ei=10.549, th_e=8.295, kf=-1, e_arm='SHMS', case='case1', verbose=True)
     
-  
+
     #------ CaFe heep for proton absorption------
     #calc_h2(Ei=10.6, th_e=-1, kf=7, e_arm='SHMS', case='case2', verbose=True)
     #calc_h2(Ei=10.3, th_e=8.3, kf=-1, e_arm='SHMS', case='case1', verbose=True)
@@ -231,4 +234,4 @@ if __name__ == "__main__":
     #calc_h2(Ei=10.549, th_e=-1, kf=8.895, e_arm='SHMS', case='case2', verbose=True)  # delta = +4 %
     #calc_h2(Ei=10.549, th_e=-1, kf=9.235, e_arm='SHMS', case='case2', verbose=True)  # delta = +8 %
     #calc_h2(Ei=10.549, th_e=-1, kf=9.577, e_arm='SHMS', case='case2', verbose=True)  # delta = +12 %
-    calc_h2(Ei=10.549, th_e=-1, kf=9.919, e_arm='SHMS', case='case2', verbose=True)  # delta = +16 %
+    #calc_h2(Ei=10.549, th_e=-1, kf=9.919, e_arm='SHMS', case='case2', verbose=True)  # delta = +16 %
