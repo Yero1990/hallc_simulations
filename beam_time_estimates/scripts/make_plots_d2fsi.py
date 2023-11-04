ed8e29d37c3ef20e3c91041feaa1449ee8a28e3b
Carlos Yero <cyero@jlab.org>
2023-11-04 19:02:26 -0400

    updated

--- beam_time_estimates/scripts/make_plots_d2fsi.py
+++ beam_time_estimates/scripts/make_plots_d2fsi.py
@@ -7,18 +7,39 @@ from uncertainties import unumpy
 # This script reads both (pwia, fsi) for a given th_rq setting, and overlays the
 # FSI / PWIA for all the central thrq_c, binned in missing momentum (pm_bin)
 
-thrq_c = [28, 49, ] #deg
+plot_flag = 'ratio'
+#plot_flag = 'ratio_rel_err'
+#plot_flag = 'overlay'
+
+#thrq_c = [49, 60, 72] #[28, 49, 55, 60, 66, 72, 79 ] #deg
+thrq_c = [28, 49, 55, 60, 66, 72, 79 ] #deg
+
+#thrq_c = [60 ] #deg
+
 pm_c = [800] # MeV/c
 
+# CREATE FIGURE FOR  INDIVIDUAL PLOTS
+#plt.figure(1)
 
+# set figure subplots for overlay of pwia and fsi
 fig, ax = plt.subplots(5, 8, sharex='col', sharey='row')
 fig.text(0.5, 0.01, r'Recoil Angle $\theta_{rq}$ [deg]', ha='center')
-fig.text(0.01, 0.5, r'R = FSI / PWIA', va='center', rotation='vertical')
+fig.text(0.01, 0.5, r'FSI (or PWIA)', va='center', rotation='vertical')
 fig.set_size_inches(14,8, forward=True)
-    
+
+# set figure subplots for ratio
+fig_ratio, ax_ratio = plt.subplots(5, 8, sharex='col', sharey='row')
+fig_ratio.text(0.5, 0.01, r'Recoil Angle $\theta_{rq}$ [deg]', ha='center')
+fig_ratio.text(0.01, 0.5, r'R = FSI / PWIA', va='center', rotation='vertical')
+fig_ratio.set_size_inches(14,8, forward=True)
+
+# scale counts by hours
+scale = 168.
 # loop over central recoil angles files for a given central momentum
 for i in thrq_c:
-    
+
+    rel_err_thrs = 0.3 # mask >30 % relative error
+
     # read dataframe
     df_fsi = pd.read_csv('d2_pm800_thrq%i_fsi_rad_yield_80.0uA_1.0hr.csv'%(i), comment='#')
     df_pwia = pd.read_csv('d2_pm800_thrq%i_pwia_rad_yield_80.0uA_1.0hr.csv'%(i), comment='#')
@@ -28,33 +49,85 @@ for i in thrq_c:
 
     pm_binw = (df_fsi.yup[0] - df_fsi.ylow[0])
     
-    fsi_N = df_fsi.zcont
-    fsi_Nerr = df_fsi.zcont_err
+    fsi_N = df_fsi.zcont * scale
+    #fsi_Nerr = df_fsi.zcont_err * scale  # error based on weight
+    fsi_Nerr = np.sqrt(fsi_N)     # error based on sqrt(N)
 
-    pwia_N = df_pwia.zcont
-    pwia_Nerr = df_pwia.zcont_err
+    fsi_rel_err = fsi_Nerr / fsi_N
 
+    pwia_N = df_pwia.zcont * scale
+    #pwia_Nerr = df_pwia.zcont_err * scale    # error based on weight
+    pwia_Nerr = np.sqrt(pwia_N)              # error based on sqrt(N)
+    pwia_rel_err = pwia_Nerr / pwia_N         
+
+    pwia_N = ma.masked_where(pwia_N==0, pwia_N)
+    pwia_Nerr = ma.masked_where(pwia_N==0, pwia_Nerr)
+
+    fsi_N = ma.masked_where(fsi_N==0, fsi_N)
+    fsi_Nerr = ma.masked_where(fsi_N==0, fsi_Nerr)
+    
+    
     ratio = fsi_N / pwia_N
     ratio_err = ratio * np.sqrt((fsi_Nerr/fsi_N)**2 + (pwia_Nerr/pwia_N)**2)
 
-    rel_err = ratio_err / ratio
+    ratio_rel_err = ratio_err / ratio
 
-    rel_err_thrs = 0.3 # mask >30 % relative error
-    ratio = ma.masked_where(rel_err>rel_err_thrs, ratio)
-    ratio_err = ma.masked_where(rel_err>rel_err_thrs, ratio_err)
+    ratio = ma.masked_where(ratio_rel_err>rel_err_thrs, ratio)
+    ratio_err = ma.masked_where(ratio_rel_err>rel_err_thrs, ratio_err)
 
 
     for idx, pm_bin in enumerate(pm_bins):
-        print(idx)
 
-        ax = plt.subplot(5, 8, idx+1)
-        ax.errorbar(thrq_bin[df_fsi.y0==pm_bin], ratio[df_fsi.y0==pm_bin], ratio_err[df_fsi.y0==pm_bin], marker='o', linestyle='None')
+        # ---- plot individual figures ------
+        #if pm_bin == 0.84:
+            
+        #    plt.figure(1)
+        #    plt.errorbar(thrq_bin[df_fsi.y0==pm_bin], pwia_N[df_fsi.y0==pm_bin], pwia_Nerr[df_fsi.y0==pm_bin], marker='o', linestyle='None', mfc='black', mec='black', ecolor='black', label='PWIA')
+        #    plt.errorbar(thrq_bin[df_fsi.y0==pm_bin], fsi_N[df_fsi.y0==pm_bin], fsi_Nerr[df_fsi.y0==pm_bin], marker='^', linestyle='None', mfc='red', mec='red', ecolor='red', label='FSI')
 
-        if i==thrq_c[0]:
-            ax.set_title(r"%.2f $\pm$ %.2f MeV"%(pm_bin, pm_binw/2.), fontsize=10)
+        if plot_flag=='ratio':
+            # ---- plot ratio fsi/pwia -----
+            ax_ratio = plt.subplot(5, 8, idx+1)
+            ax_ratio.errorbar(thrq_bin[df_fsi.y0==pm_bin], ratio[df_fsi.y0==pm_bin], ratio_err[df_fsi.y0==pm_bin], marker='o', linestyle='None', label=r'$\theta_{rq}=%.1f$'%i)
+            if i==thrq_c[0]:
+                ax_ratio.set_title(r"%.0f $\pm$ %.0f MeV"%(pm_bin*1000, pm_binw*1000/2.), fontsize=10)
+
+        if plot_flag=='ratio_rel_err':
+            # ---- plot ratio fsi/pwia -----
+            y_const = np.zeros(len(ratio_rel_err[df_fsi.y0==pm_bin]))
+            yerr = ratio_rel_err[df_fsi.y0==pm_bin]
+            x = thrq_bin[df_fsi.y0==pm_bin]
+            
+            y_const_m = ma.masked_where(ratio_rel_err[df_fsi.y0==pm_bin]>rel_err_thrs, y_const)
+            yerr_m = ma.masked_where(ratio_rel_err[df_fsi.y0==pm_bin]>rel_err_thrs, yerr)
+            x_m = ma.masked_where(ratio_rel_err[df_fsi.y0==pm_bin]>rel_err_thrs, x)
+            print('len_yc:',len(y_const_m))
+            print('len_yerr:',len(yerr_m))
+            print('len_x:',len(x_m))
+            
+            ax_ratio = plt.subplot(5, 8, idx+1)
+            ax_ratio.errorbar(x_m, y_const_m, yerr_m, marker='o', linestyle='None', label=r'$\theta_{rq}=%.1f$'%i)
+
+            plt.axhline(y = 0.15, color = 'r', linestyle = '--')
+            plt.axhline(y = -0.15, color = 'r', linestyle = '--')
+            
+            if i==thrq_c[0]:
+                ax_ratio.set_title(r"%.0f $\pm$ %.0f MeV"%(pm_bin*1000, pm_binw*1000/2.), fontsize=10)
+
+                
+        if plot_flag=='overlay':
+            ax = plt.subplot(5, 8, idx+1)
+            ax.errorbar(thrq_bin[df_fsi.y0==pm_bin], pwia_N[df_fsi.y0==pm_bin], pwia_Nerr[df_fsi.y0==pm_bin], marker='o', linestyle='None', mfc='None', mec='black', ecolor='black', label=r'$\theta_{rq}=%.1f$'%i)
+            ax.errorbar(thrq_bin[df_fsi.y0==pm_bin], fsi_N[df_fsi.y0==pm_bin], fsi_Nerr[df_fsi.y0==pm_bin], marker='^', linestyle='None', mfc='None', mec='red', ecolor='red')
+            if i==thrq_c[0]:
+                ax.set_title(r"%.2f $\pm$ %.2f MeV"%(pm_bin, pm_binw/2.), fontsize=10)
 
 
+
+
+            
 plt.tight_layout()
+plt.legend()
 plt.show()
 
   
